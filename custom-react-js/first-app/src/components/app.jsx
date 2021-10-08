6cd8e10a6c3a98aa326fd7e5981e461ac4719893
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-09 03:00:11 +0600

    Update codes

---
+++ custom-react-js/first-app/src/components/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+
+import Import from './import'
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="text-center">
+                    <h2>Bismillahir Rahmanir Rahim</h2>
+                    <p>Ever tried, Ever failed, Try Again. Fail Again, Fail better</p>
+                </div>
+                <hr />
+                <br />
+
+                {/* Import */}
+                
+                <Import />
+
+                
+            </div>
+        )
+    }
+}
+
+
+export default App
