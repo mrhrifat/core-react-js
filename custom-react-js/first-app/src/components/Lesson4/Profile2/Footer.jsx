2f9590b1ef5415b6bd29d2d7e907bd5e7008eba7
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-10 14:58:49 +0600

    Split Component

---
+++ custom-react-js/first-app/src/components/Lesson4/Profile2/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Footer = () => (
+    <footer className="students">
+        <h6>Students</h6>
+        <ul className="list-unstyled">
+            <li>
+                <a href="#">
+                    Ibnul Jawhi
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Ibn Kayyim
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Ibn Kasir
+                </a>
+            </li>
+        </ul>
+    </footer>
+)
+export default Footer
