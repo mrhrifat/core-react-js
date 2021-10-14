ee69fe7478af8404e56b4330a36d45c914b82a4e
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-14 15:12:36 +0600

    Update 2 new components

--- custom-react-js/first-app/src/components/import.jsx
+++ custom-react-js/first-app/src/components/import.jsx
@@ -5,11 +5,15 @@ import Lesson1 from './Lesson1/Lesson1'
 import Lesson2 from './Lesson2/Lesson2'
 import Lesson3 from './Lesson3/Lesson3'
 import Lesson4 from './Lesson4/Lesson4'
+import Props from './Lesson4/Props'
+import ChildrenProps from './Lesson4/Children'
 
 class Import extends React.Component {
     render() {
         return (
             <div>
+                <Props />
+                <ChildrenProps />
                 <Lesson1 />
                 <Lesson2 />
                 <Lesson3 />
