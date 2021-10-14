fbdfc005af8fb64dfb01238a9cb5af0024cc791d
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-15 00:42:01 +0600

    Upadte with new files

--- custom-react-js/first-app/src/components/import.jsx
+++ custom-react-js/first-app/src/components/import.jsx
@@ -5,19 +5,23 @@ import Lesson1 from './Lesson1/Lesson1'
 import Lesson2 from './Lesson2/Lesson2'
 import Lesson3 from './Lesson3/Lesson3'
 import Lesson4 from './Lesson4/Lesson4'
-import Props from './Lesson4/Props'
-import ChildrenProps from './Lesson4/Children'
+
+
+import Lesson5 from './Lesson5/Lesson5'
 
 class Import extends React.Component {
     render() {
         return (
             <div>
-                <Props />
-                <ChildrenProps />
+                {/*
                 <Lesson1 />
                 <Lesson2 />
                 <Lesson3 />
                 <Lesson4 />
+                */}
+
+
+                <Lesson5 />
             </div>
         )
     }
