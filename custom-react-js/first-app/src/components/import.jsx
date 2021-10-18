eb9c26f6a200d6a7efcb523d72599353aa5aea8a
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-19 01:42:12 +0600

    Update with new components

--- custom-react-js/first-app/src/components/import.jsx
+++ custom-react-js/first-app/src/components/import.jsx
@@ -5,9 +5,9 @@ import Lesson1 from './Lesson1/Lesson1'
 import Lesson2 from './Lesson2/Lesson2'
 import Lesson3 from './Lesson3/Lesson3'
 import Lesson4 from './Lesson4/Lesson4'
-
-
 import Lesson5 from './Lesson5/Lesson5'
+import Lesson6 from './Lesson6/Lesson6'
+import Master from './Master/Master'
 
 class Import extends React.Component {
     render() {
@@ -18,10 +18,15 @@ class Import extends React.Component {
                 <Lesson2 />
                 <Lesson3 />
                 <Lesson4 />
+                <Lesson5 />
+                <Master />
+                <Lesson6 />
                 */}
 
 
-                <Lesson5 />
+
+
+
             </div>
         )
     }
