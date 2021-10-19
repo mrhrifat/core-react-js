eeb3823eaccdff4a4dc1da58247b271dd3e45c26
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-20 00:50:41 +0600

    Update components

---
+++ custom-react-js/first-app/src/components/Lesson8/Lesson8.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Form from "./Form/Form";
+import Form2 from "./Form2/Form2";
+import Form3 from "./Form3/Form3";
+
+
+class Lesson8 extends Component {
+    render() {
+        return (
+            <div>
+                <h6>Lesson 8</h6>
+                {/*
+                 <Form /> 
+                 <Form2 />
+                 */}
+
+                <Form3 />
+
+            </div>
+        )
+    }
+}
+
+export default Lesson8
