4b2dca695e01bceadbf5a74a8c32f72e3dec5230
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-22 19:53:03 +0600

    Update components path

--- custom-react-js/first-app/src/components/Lesson8/Lesson8.jsx
+++ custom-react-js/first-app/src/components/Lesson8/Lesson8.jsx
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import Form from "./Form/Form";
 import Form2 from "./Form2/Form2";
 import Form3 from "./Form3/Form3";
+import Form4 from "./Form4/Form4";
+import UncontroledForm from "./UncontroledForm/UncontroledForm";
+import ControledForm from "./ControledForm/ControledForm";
+import SplitForm from "./SplitForm/SplitForm";
+import SplitForm2 from "./SplitForm2/SplitForm2";
+import SplitForm3 from "./SplitForm3/SplitForm3";
+import SplitForm4 from "./SplitForm4/SplitForm4";
+import SplitForm5 from "./SplitForm5/SplitForm5";
+import SplitForm6 from "./SplitForm6/SplitForm6";
 
 
 class Lesson8 extends Component {
@@ -10,11 +19,25 @@ class Lesson8 extends Component {
             <div>
                 <h6>Lesson 8</h6>
                 {/*
-                 <Form /> 
-                 <Form2 />
+
+                <Form /> 
+                <Form2 />
+                <Form3 />
+                <Form4 />
+                <UncontroledForm />
+                <ControledForm />
+                <SplitForm />
+                <SplitForm2 />
+                <SplitForm3 />
+                <SplitForm4 />
+                <SplitForm5 />
+
                  */}
 
-                <Form3 />
+
+
+                <SplitForm6 />
+
 
             </div>
         )
