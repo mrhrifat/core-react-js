723f089500a551f1e7de25be48ef4ea2e16920f3
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-22 20:50:06 +0600

    Update files

--- custom-react-js/first-app/src/components/import.jsx
+++ custom-react-js/first-app/src/components/import.jsx
@@ -7,6 +7,8 @@ import Lesson3 from './Lesson3/Lesson3'
 import Lesson4 from './Lesson4/Lesson4'
 import Lesson5 from './Lesson5/Lesson5'
 import Lesson6 from './Lesson6/Lesson6'
+import Lesson7 from './Lesson7/Lesson7'
+import Lesson8 from './Lesson8/Lesson8'
 import Master from './Master/Master'
 
 class Import extends React.Component {
@@ -21,9 +23,11 @@ class Import extends React.Component {
                 <Lesson5 />
                 <Master />
                 <Lesson6 />
+                <Lesson7 />
                 */}
 
 
+                <Lesson8 />
 
 
 
