a5706479fcb03228b8badd50fa5684f2b561e9fa
mrhrifat <mrhrifat383759@gmail.com>
2022-01-06 15:43:47 +0600

    Remove custom react pack

--- react-js/src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "../styles/styles.css";
-import "../styles/styles.scss";
-
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Mrh Rifat" />, mountNode);
